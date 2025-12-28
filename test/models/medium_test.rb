f0cb2c717eccb04265236b6c0552fb2529adf842
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2025-12-27 20:26:53 -0500

    medium model tests added

--- test/models/medium_test.rb
+++ test/models/medium_test.rb
@@ -1,6 +1,21 @@
 require "test_helper"
 
 class MediumTest < ActiveSupport::TestCase
+  should have_many(:characters)
+  should have_many(:user_media)
+  should have_many(:users).through(:user_media)
+
+  should allow_value(1).for(:media_type)
+  should allow_value(3).for(:media_type)
+  should allow_value(4).for(:media_type)
+  should allow_value(5).for(:media_type)
+  should allow_value(6).for(:media_type)
+
+  should allow_value("animanga").for(:media_type)
+  should allow_value("game").for(:media_type)
+  should allow_value("cartoon").for(:media_type)
+  should allow_value("movie").for(:media_type)
+  should allow_value("tv_show").for(:media_type)
   # test "the truth" do
   #   assert true
   # end
