013b4db39c81bbbbf2cf430d6820526ff596b4e0
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2025-12-27 20:25:39 -0500

    character model tests added

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -2,6 +2,22 @@ require "test_helper"
 
 class CharacterTest < ActiveSupport::TestCase
   should belong_to(:medium)
+
+  should have_many(:user_characters)
+  should have_many(:users).through(:user_characters)
+
+  should have_many(:character_items)
+  should have_many(:items).through(:character_items)
+
+  should allow_value(1).for(:hair_length)
+  should allow_value(2).for(:hair_length)
+  should allow_value(3).for(:hair_length)
+  should allow_value(4).for(:hair_length)
+
+  should allow_value("short").for(:hair_length)
+  should allow_value("bob").for(:hair_length)
+  should allow_value("medium").for(:hair_length)
+  should allow_value("long").for(:hair_length)
   # test "the truth" do
   #   assert true
   # end
