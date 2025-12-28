5c7f09a50f7cd60f637081b3d809a65b3cc1fbc7
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2025-12-27 22:41:19 -0500

    rating model tests added

--- test/models/user_item_test.rb
+++ test/models/user_item_test.rb
@@ -8,6 +8,14 @@ class UserItemTest < ActiveSupport::TestCase
   # Test Validations
   should validate_presence_of(:user_id)
   should validate_presence_of(:item_id)
+  should validate_presence_of(:purchased)
+
+  should allow_value(nil).for(:rating)
+  should allow_value("one_star").for(:rating)
+  should allow_value("two_stars").for(:rating)
+  should allow_value("three_stars").for(:rating)
+  should allow_value("four_stars").for(:rating)
+  should allow_value("five_stars").for(:rating)
   # test "the truth" do
   #   assert true
   # end
