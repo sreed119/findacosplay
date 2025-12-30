701698253501e3efa7d91e7283140320dbda66ae
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2025-12-30 01:58:59 -0500

    character item association filters character options

--- app/helpers/character_items_helper.rb
+++ app/helpers/character_items_helper.rb
@@ -1,6 +1,12 @@
 module CharacterItemsHelper
-  def get_character_item_options
-    Character.alphabetical.map { |c| [ "#{c.name} (#{c.medium.name})", c.id ] }
+  def get_character_item_options(item_id = nil)
+    if item_id
+      # characters that aren't already associated with this item
+      associated_character_ids = CharacterItem.where(item_id: item_id).select(:character_id)
+      Character.alphabetical.where.not(id: associated_character_ids).map { |c| [ "#{c.name} (#{c.medium.name})", c.id ] }
+    else
+      Character.alphabetical.map { |c| [ "#{c.name} (#{c.medium.name})", c.id ] }
+    end
   end
 
   def get_item_options
