a2e4e2535586fd27cc05bac0097af40a11e62386
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2025-12-30 21:30:13 -0500

    react on rails fully installed

---
+++ config/webpack/development.js
@@ -0,0 +1,22 @@
+// The source code including full typescript support is available at: 
+// https://github.com/shakacode/react_on_rails_demo_ssr_hmr/blob/master/config/webpack/development.js
+
+const { devServer, inliningCss } = require('shakapacker');
+
+const generateWebpackConfigs = require('./generateWebpackConfigs');
+
+const developmentEnvOnly = (clientWebpackConfig, _serverWebpackConfig) => {
+  // React Refresh (Fast Refresh) setup - only when webpack-dev-server is running (HMR mode)
+  // This matches the condition in generateWebpackConfigs.js and babel.config.js
+  if (process.env.WEBPACK_SERVE) {
+    // eslint-disable-next-line global-require
+    const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+    clientWebpackConfig.plugins.push(
+      new ReactRefreshWebpackPlugin({
+        // Use default overlay configuration for better compatibility
+      }),
+    );
+  }
+};
+
+module.exports = generateWebpackConfigs(developmentEnvOnly);
