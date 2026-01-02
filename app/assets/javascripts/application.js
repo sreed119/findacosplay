3081bc29e0b3f3a4ebd5eb9aa46e5d38fdf93d79
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2026-01-02 00:12:55 -0500

    hamburger menu for mobile added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,21 @@
 //= require jquery_ujs
 //= require materialize
 
+// Toggle menu function (global scope for onclick handler)
+function toggleMenu() {
+   var x = document.getElementById("hamburger-content");
+   if (x.style.display === "block") {
+      x.style.display = "none";
+   } else {
+      x.style.display = "block";
+   }
+}
+
+$("#hamburger-toggle").on("click", function(event){
+   toggleMenu();
+});
+
+
 // Flash fade
 $(function() {
    $('.alert-box').fadeIn('normal', function() {
@@ -18,6 +33,5 @@ $(function() {
       };
       new TomSelect(el,settings);
    });
-
 
 });
