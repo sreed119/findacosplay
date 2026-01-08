878825eb9af12de1557f86e05a1bb09eda868d3a
Stephanie Reed <156985622+sreed119@users.noreply.github.com>
2026-01-08 10:58:39 -0500

    tom select issue resolved

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,25 +34,19 @@ $("#dropdown_trigger").on("click", function(event){
 });
 
 $(document).ready(function(){
-    $('.halfway-fab').floatingActionButton();
-  });
-
-
-// Flash fade
-$(function() {
-   $('.alert-box').fadeIn('normal', function() {
-      $(this).delay(2000).fadeOut();
-   });
-   
-   document.querySelectorAll('.responsive_search').forEach((el)=>{
-      let settings = {
-         create: true,
-         sortField: {
-            field: "text",
-            direction: "asc"
-         }
-      };
-      new TomSelect(el,settings);
-   });
-
+    // Flash fade
+    $('.alert-box').fadeIn('normal', function() {
+       $(this).delay(2000).fadeOut();
+    });
+    
+    document.querySelectorAll('.responsive_search').forEach((el)=>{
+       let settings = {
+          create: true,
+          sortField: {
+             field: "text",
+             direction: "asc"
+          }
+       };
+       new TomSelect(el,settings);
+    });
 });
